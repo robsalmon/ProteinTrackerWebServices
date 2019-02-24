c39b732d76aca73c43e257ed152156a35f30bb19
Robert Salmon <robdavidsalmon@msn.com>
2019-02-24 10:49:39 +0000

    Replace Sessions with repository pattern for users

---
+++ ProteinTrackerWebServices/ProteinTrackingService.asmx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ProteinTrackerWebServices
+{
+    /// <summary>
+    /// Summary description for ProteinTrackingService
+    /// </summary>
+    [WebService(Namespace = "http://www.robsalmon.me.uk")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+ 
+    public class ProteinTrackingService : WebService {
+
+        private UserRepository repository = new UserRepository();
+
+
+      [WebMethod(Description = "Add an amount to the total", EnableSession = true)]
+      public int AddProtein(int amount, int userId)
+        {
+
+            var user = repository.GetById(userId);
+            
+            if (user == null)
+            {
+                return -1;
+            }
+
+            user.Total += amount;
+            repository.Save(user);
+
+            return user.Total;
+
+        }
+
+        [WebMethod(Description = "Add a new user", EnableSession = true)]
+        public int AddUser(string name, int goal)
+        {
+            var user = new User { Goal = goal, Name = name, Total = 0 };
+
+            repository.Add(user);
+
+            return user.UserId;
+
+        }
+
+        [WebMethod(Description = "Return a list of users", EnableSession = true)]
+        public List<User> ListUsers()
+        {
+            return new List<User>(repository.GetAll());
+        }
+    }
+}
---
+++ ProteinTrackerWebServices/UserRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinTrackerWebServices
+{
+    interface IUserRepository
+    {
+
+    }
+
+    public class UserRepository : IUserRepository
+    {
+
+        private static readonly List<User> users = new List<User>();
+
+        private static int nextId = 0;
+
+
+        public void Add(User user)
+        {
+            user.UserId = nextId;
+
+            nextId++;
+
+            users.Add(user);
+        }
+
+        public IReadOnlyCollection<User> GetAll()
+        {
+            return users.AsReadOnly();
+        }
+
+        public User GetById(int id)
+        {
+            User user = users.SingleOrDefault(u => u.UserId == id);
+
+            if (user == null)
+            {
+                return null;
+            }
+            return new User { Goal = user.Goal, Name = user.Name, UserId = user.UserId, Total = user.Total };
+        }
+
+        public void Save(User updatedUser)
+        {
+            User originalUser = users.SingleOrDefault(u => u.UserId == updatedUser.UserId);
+
+            if (originalUser == null)
+            {
+                return;
+            }
+
+            originalUser.Name = updatedUser.Name;
+            originalUser.Total = updatedUser.Total;
+            originalUser.Goal = updatedUser.Goal;
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
